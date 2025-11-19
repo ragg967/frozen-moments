e73e2a182aa5cd623524bec995f8a13b725f268b
Ragg967 <rywatson1027@gmail.com>
2025-11-19 15:01:10 -0800

    added peak

--- scrpits/main.gd
+++ scrpits/main.gd
@@ -1,7 +1,9 @@
 extends Node
 
+const PEAKS = ["res://assets/borgurbirby.jpg", "res://assets/gay yuri.jpg", "res://assets/icon.svg", "res://assets/images (2).jpg", "res://assets/korbi.jpg", "res://assets/Screenshot 2025-11-19 at 14-30-02 11_5 GWC GAME JAM FROZEN MOMENTS - Google Docs.png"]
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	if !ResourceLoader.exists("res://assets/Screenshot 2025-11-19 at 14-30-02 11_5 GWC GAME JAM FROZEN MOMENTS - Google Docs.png"):
-		OS.crash("You deleted peak")
+	for peak in PEAKS:
+		if !ResourceLoader.exists(peak):
+			OS.crash("You deleted peak")
